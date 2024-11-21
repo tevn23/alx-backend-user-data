2a5c356bd2fb780d6d2e97d11cdd333ec12d59f2
tevn23 <mabulatt23@gmail.com>
2024-11-21 06:12:07 +0200

    Created user model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 # Base class for SQLAlchemy models
 Base = declarative_base()
 
+
 class User(Base):
     """
     SQLAlchemy model for the 'users' table.
