b3aba7b8fb9a87efd2b96dee0ace1a4b5fd2818e
tevn23 <mabulatt23@gmail.com>
2024-11-21 06:11:17 +0200

    Created user model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+User model definition for a SQLAlchemy database.
+"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+# Base class for SQLAlchemy models
+Base = declarative_base()
+
+class User(Base):
+    """
+    SQLAlchemy model for the 'users' table.
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
